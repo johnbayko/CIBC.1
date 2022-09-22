a9dad6883664af51c32e2cee9df887503c008d86
John Bayko <jbayko@sasktel.net>
2022-09-21 19:46:04 -0600

    Implement the time getting part

--- timeserv.go
+++ timeserv.go
@@ -2,35 +2,115 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
     "net"
     "net/http"
     "strings"
+    "time"
 )
 
 type timeHandler struct {
-    // Might need caching here.
+    // Timezone caching can go here if needed.
 }
 
+
+func timeErrorHandle(w http.ResponseWriter, statusCode int, message string) {
+    w.WriteHeader(statusCode)
+    fmt.Fprintln(w, message)
+    // When logging is added, add err parameter and do that here.
+}
+
+
+func getTzNameList(r *http.Request) []string {
+    var tzNameList = []string{}
+
+    var query = r.URL.Query()
+    var tzName, hasTZName = query["tz"]
+    if hasTZName {
+        tzNameList = strings.Split(tzName[0], ",")
+    }
+    return tzNameList
+}
+
+
+func getTzLocMap(tzNameList []string) (map[string]*time.Location, string, error) {
+    tzLocMap := map[string]*time.Location {}
+
+    if len(tzNameList) == 0 {
+        tzLocMap["current_time"] = time.UTC
+    } else if len(tzNameList) == 1 {
+        // Only one timezone, key is same as no timezone.
+        tzName := tzNameList[0]
+        tzLoc, tzLocErr := time.LoadLocation(tzName)
+        if tzLocErr != nil {
+            return nil, "", tzLocErr
+        }
+        tzLocMap["current_time"] = tzLoc
+    } else {
+        // Multiple time zones, key by timezone name.
+        for _, tzName := range tzNameList {
+            tzLoc, tzLocErr := time.LoadLocation(tzName)
+            if tzLocErr != nil {
+                return nil, ": " + tzName, tzLocErr
+            }
+            tzLocMap[tzName] = tzLoc
+        }
+    }
+    return tzLocMap, "", nil
+}
+
+
+func getTimeMap(tzMap map[string]*time.Location) map[string]string {
+    timeMap := map[string]string {}
+
+    now := time.Now()
+    for tzName, tzLoc := range tzMap {
+        timeString := now.In(tzLoc).Format("2006-01-02 15:04:05 -0700")
+        timeMap[tzName] = timeString
+    }
+    return timeMap
+}
+
+
+func (h *timeHandler) timeGetHandle(w http.ResponseWriter, r *http.Request) {
+    // Timezones from parameters (if any).
+    var tzNameList = getTzNameList(r)
+
+    // Map timezone names to timezone locations.
+    tzMap, tzName, tzMapErr := getTzLocMap(tzNameList)
+    if tzMapErr != nil {
+        timeErrorHandle(w, http.StatusNotFound, "invalid timezone" + tzName)
+        return
+    }
+
+    // Map timezone names to formatted times.
+    timeMap := getTimeMap(tzMap)
+
+    // Convert timezones to JSON message.
+    trJson, trJsonErr := json.MarshalIndent(timeMap, "", "  ")
+    if trJsonErr != nil {
+        timeErrorHandle(w, http.StatusInternalServerError, "Could not encode time.")
+        return
+    }
+
+    // Write response.
+    w.WriteHeader(http.StatusOK)
+    w.Write(trJson)
+    fmt.Fprintln(w, "")  // newline
+}
+
+
 func (h *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("r.Method", r.Method)  // debug
     switch {
     case r.Method == "GET":
-       var tzNameList = []string{}
-       {
-           var query = r.URL.Query()
-           var tzName, hasTZName = query["tz"]
-           if hasTZName {
-               tzNameList = strings.Split(tzName[0], ",")
-           }
-       }
-       for tzNameIdx, tzName := range tzNameList {
-           fmt.Println("tzNameIdx", tzNameIdx, "tzName", tzName)  // debug
-       }
+        h.timeGetHandle(w, r)
     }
 }
 
+
 func main() {
-    fmt.Println("start")
+    // Don't know yet what port this is assigned to,
+    // let OS allocate one and display it.
     listener, err := net.Listen("tcp", ":0")
     if err != nil {
         panic(err)
--- timeserv_test.sh
+++ timeserv_test.sh
@@ -4,17 +4,26 @@ port=0
 if [ "$#" -ge 1 ]
 then
     port=$1
+    echo port ${port}
 else
     read "port?Port:"
 fi
 
-echo port ${port}
-
 echo GET time
 curl http://localhost:${port}/api/time
 
-echo GET time tz=abc
-curl "http://localhost:${port}/api/time?tz=abc"
+echo GET time tz=America/New_York
+curl "http://localhost:${port}/api/time?tz=America/New_York"
 
-echo GET time tz=abc, def
-curl "http://localhost:${port}/api/time?tz=abc,def"
+echo GET time tz=America/New_York, Asia/Kolkata
+curl "http://localhost:${port}/api/time?tz=America/New_York,Asia/Kolkata"
+
+echo GET time tz=X
+curl "http://localhost:${port}/api/time?tz=X"
+
+echo GET time tz=X, Asia/Kolkata
+curl "http://localhost:${port}/api/time?tz=X,Asia/Kolkata"
+
+echo GET time tz=America/New_York, X
+curl "http://localhost:${port}/api/time?tz=America/New_York,X"
+
